a04a25fdc03bcbdcda5516e92c0e9a13d2413bb3
Daviks <fedaloor_0605@hotmail.es>
2015-09-21 00:33:15 -0500

    Actualización login y logout

--- mvc/objetos/login.php
+++ mvc/objetos/login.php
@@ -8,16 +8,15 @@ $loginCollectorObj = new loginCollector();
 $objeto = $loginCollectorObj->ingreso($usuario, $password);
 $res = $objeto['comprobador'];
 $identificador = $objeto['identificador'];
+
 if($res === true){
-    $_SESSION["newsession"] = $usuario;
+    $_SESSION["newsession"] = $identificador;
     echo $_SESSION["newsession"];
     header('Location: ../../index.php');
-    return $identificador;
 }else{
     print '<script language="JavaScript">'; 
     print 'alert("Error, usuario o contraseña incorrectos");'; 
     print '</script>';
     header('Location: ../../index.php');
-    return $identificador;
 }
 ?>
